1c6657c66c09475ec25db147d1745f70f515aaf9
Vladimir <vovanpopkov@mail.ru>
2018-02-13 01:13:39 +0300

    Как будто бы работает, но что то херня какая-то.

--- src/com/company/Leg.java
+++ src/com/company/Leg.java
@@ -2,14 +2,25 @@ package com.company;
 
 public class Leg extends Thread{
 
-    int stepLenght;
+    private int stepLenght;
+    private int numberOfLeg;
 
     @Override
     public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            stepLenght = (int) (1 + Math.random() * 5);
+        }
     }
 
-    public int makeStep(){
-        stepLenght = (int) (1 + Math.random() * 5);
+    public int getStepLenght(){
         return stepLenght;
+    }
+
+    public int getNumberOfleg(){
+        return numberOfLeg;
+    }
+
+    public void setNumberOfLeg(int numberOfLeg){
+        this.numberOfLeg = numberOfLeg;
     }
 }
--- src/com/company/Robot.java
+++ src/com/company/Robot.java
@@ -6,23 +6,44 @@ import java.util.List;
 public class Robot {
 
     private int distance;
+    private int currentLeg = 1;
     private int legs;
-    private int currentLeg;
+    private Object synch = new Object();
     private int steps;
     private List<Leg> legList = new ArrayList<>();
 
-    public Robot(int distance, int legs){
+    public Robot(int distance, int legs) {
         this.distance = distance;
         this.legs = legs;
         for (int i = 0; i < legs; i++) {
             Leg leg = new Leg();
+            leg.setNumberOfLeg(i + 1);
             legList.add(leg);
             leg.start();
         }
     }
 
-    public int start(){
-
+    public int start() {
+            while (distance >= 0) {
+                for (Leg leg : legList) {
+                    synchronized (synch) {
+                        if (distance >= 0 /*&& currentLeg == leg.getNumberOfleg()*/) {
+                            distance = distance - leg.getStepLenght();
+                            System.out.println("Нога " + leg.getNumberOfleg() + " сделала шаг");
+                            System.out.println("Осталось пройти " + distance);
+                            steps++;
+                            if (currentLeg != legs) {
+                                currentLeg++;
+                            } else {
+                                currentLeg = 1;
+                            }
+                        }
+                    }
+                }
+            }
+        for (Leg leg : legList) {
+            leg.interrupt();
+        }
         return steps;
     }
 }
