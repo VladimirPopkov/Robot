38f00aa99a54b3b2d72d7e88091ed10d0ecc921d
Vladimir <vovanpopkov@mail.ru>
2018-02-28 23:44:10 +0300

    Начат процесс переделки.

--- src/com/company/Leg.java
+++ src/com/company/Leg.java
@@ -1,26 +1,36 @@
 package com.company;
 
-public class Leg extends Thread{
+public class Leg extends Thread {
 
     private int stepLenght;
     private int numberOfLeg;
+    private Robot robot;
+
+    public Leg(Robot robot) {
+        this.robot = robot;
+    }
 
     @Override
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
-            stepLenght = (int) (1 + Math.random() * 5);
+            synchronized (robot) {
+                if (robot.getCurrentLeg() == numberOfLeg) {
+                    System.out.println("Нога " + numberOfLeg + " шагнула");
+                    stepLenght = (int) (1 + Math.random() * 5);
+                }
+            }
         }
     }
 
-    public int getStepLenght(){
+    public int getStepLenght() {
         return stepLenght;
     }
 
-    public int getNumberOfleg(){
+    public int getNumberOfleg() {
         return numberOfLeg;
     }
 
-    public void setNumberOfLeg(int numberOfLeg){
+    public void setNumberOfLeg(int numberOfLeg) {
         this.numberOfLeg = numberOfLeg;
     }
 }
--- src/com/company/Robot.java
+++ src/com/company/Robot.java
@@ -8,7 +8,6 @@ public class Robot {
     private int distance;
     private int currentLeg = 1;
     private int legs;
-    private Object synch = new Object();
     private int steps;
     private List<Leg> legList = new ArrayList<>();
 
@@ -16,7 +15,7 @@ public class Robot {
         this.distance = distance;
         this.legs = legs;
         for (int i = 0; i < legs; i++) {
-            Leg leg = new Leg();
+            Leg leg = new Leg(this);
             leg.setNumberOfLeg(i + 1);
             legList.add(leg);
             leg.start();
@@ -24,26 +23,28 @@ public class Robot {
     }
 
     public int start() {
-            while (distance >= 0) {
-                for (Leg leg : legList) {
-                    synchronized (synch) {
-                        if (distance >= 0 /*&& currentLeg == leg.getNumberOfleg()*/) {
-                            distance = distance - leg.getStepLenght();
-                            System.out.println("Нога " + leg.getNumberOfleg() + " сделала шаг");
-                            System.out.println("Осталось пройти " + distance);
-                            steps++;
-                            if (currentLeg != legs) {
-                                currentLeg++;
-                            } else {
-                                currentLeg = 1;
-                            }
-                        }
+        while (distance >= 0) {
+            for (Leg leg : legList) {
+                if (distance >= 0 /*&& currentLeg == leg.getNumberOfleg()*/) {
+                    distance = distance - leg.getStepLenght();
+                    System.out.println("Нога " + leg.getNumberOfleg() + " сделала шаг");
+                    System.out.println("Осталось пройти " + distance);
+                    steps++;
+                    if (currentLeg != legs) {
+                        currentLeg++;
+                    } else {
+                        currentLeg = 1;
                     }
                 }
             }
+        }
         for (Leg leg : legList) {
             leg.interrupt();
         }
         return steps;
+    }
+
+    public int getCurrentLeg() {
+        return currentLeg;
     }
 }
